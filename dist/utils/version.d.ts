842b6be91b76b387ce1620e29319430ac604e082
dpsifr <david@soulcraft.com>
2025-06-24 15:46:05 +0000

    Deploying to gh-pages from @ soulcraft-research/brainy@3c8f9841cc8640c871acaaa85413cffe057681ac 🚀

--- dist/utils/version.d.ts
+++ dist/utils/version.d.ts
@@ -2,5 +2,5 @@
  * This file is auto-generated during the build process.
  * Do not modify this file directly.
  */
-export declare const VERSION = "0.9.4";
+export declare const VERSION = "0.9.5";
 //# sourceMappingURL=version.d.ts.map
